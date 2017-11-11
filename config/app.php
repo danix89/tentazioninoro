7e991f9cee9d5363cdcfe61df9b432eb09d12793
Daniele Iannone <diannone3@gmail.com>
2017-11-11 19:09:23 +0100

    Aggiunta la DebugBar.

--- config/app.php
+++ config/app.php
@@ -141,11 +141,11 @@ return [
 	Illuminate\Translation\TranslationServiceProvider::class,
 	Illuminate\Validation\ValidationServiceProvider::class,
 	Illuminate\View\ViewServiceProvider::class,
-	Collective\Html\HtmlServiceProvider::class,
 	/*
 	 * Package Service Providers...
 	 */
-
+	Collective\Html\HtmlServiceProvider::class,
+	Barryvdh\Debugbar\ServiceProvider::class,
 	/*
 	 * Application Service Providers...
 	 */
@@ -201,5 +201,6 @@ return [
 	'View' => Illuminate\Support\Facades\View::class,
 	'Form' => Collective\Html\FormFacade::class,
 	'Html' => Collective\Html\HtmlFacade::class,
+	'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 ];
