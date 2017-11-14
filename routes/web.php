81e4c0da8f72e104ba543133e481c46eb3ab03ac
danix89 <>
2017-11-14 16:25:55 +0100

    Definizione della route "home".

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Tentazioninoro\User;
+use Tentazioninoro\Customer;
 
 Route::resource('user', 'UserController');
 Route::resource('customer', 'CustomerController');
@@ -22,12 +24,14 @@ Route::resource('sale-act', 'SaleActController');
 //    return view('welcome');
 //});
 
-use Tentazioninoro\User;
-use Tentazioninoro\Customer;
-Route::get('/', function () {
+Route::get('/', [ 'as' => 'home', function () { //nomino la rotta 'home'
     $users = User::get();
     $customer = Customer::get();
     Debugbar::info($users);
     Debugbar::info($customer);
     return view('welcome');
+}]);
+
+Route::get('/home', function () {
+       return redirect(route('home'));
 });
