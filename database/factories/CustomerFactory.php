42cb3a2e225b65041db4464b5efffccf8129ba62
Daniele Iannone <diannone3@gmail.com>
2018-01-21 21:00:17 +0100

    Bugfix e migliorie varie.

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -7,8 +7,8 @@ use Faker\Generator as Faker;
 $factory->define(Tentazioninoro\Customer::class, function (Faker $faker) {
     return [
         'fiscal_code' => str_random(16),
-        'mobile_phone' => $faker->phoneNumber,
-        'phone_number' => $faker->phoneNumber,
+        'phone_number_1' => $faker->phoneNumber,
+        'phone_number_2' => $faker->phoneNumber,
         'email' => $faker->unique()->safeEmail,
         'description' => $faker->sentence,
     ];
